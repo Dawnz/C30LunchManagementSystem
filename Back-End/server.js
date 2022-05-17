da74bd378ce2aff75fa35bda0f0b76a278a9d300
Michael <michael138leighton@gmail.com>
2022-05-17 13:52:52 -0500

    server connected to database

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,9 +1,20 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const mysql = require('mysql')
 
+const app = express()
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'canteen'
+})
+
+//run test to see if database is connected
+connection.connect(function(error){
+    if (!error) console.log('Canteen Database Connected')
+    else console.log(error)
+})
