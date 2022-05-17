88e5aee387e282225f649d5cfcd9027be99bce8c
Michael <michael138leighton@gmail.com>
2022-05-17 13:44:46 -0500

    requirements for the server added

---
+++ Back-End/server.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const mysql = require('mysql')
+
